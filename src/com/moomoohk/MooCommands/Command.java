aabfb56d8a0c70673ad1c3c065a72596d1a51a88
Meshulam Silk <moomoohk@ymail.com>
2013-11-24 03:56:03 +0200

    MooCommands 2.0: Completely redone

--- src/com/moomoohk/MooCommands/Command.java
+++ src/com/moomoohk/MooCommands/Command.java
@@ -1,364 +1,161 @@
 package com.moomoohk.MooCommands;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.awt.Color;
 
 /**
- * @author      Meshulam Silk <moomoohk@ymail.com>
- * @version     1.0
- * @since       2013-03-05
+ * This abstract class represents an executable command.
+ * 
+ * @author Meshulam Silk <moomoohk@ymail.com>
+ * @version 2.0
+ * @since 2013-11-24
  */
-public abstract class Command<T>
+public abstract class Command
 {
-	public static ArrayList<Command<?>> commands = new ArrayList<Command<?>>();
-	protected String command, message, help;
-	protected int minParams, maxParams;
-	protected boolean containsHTML;
-	protected T handler;
+	protected String outputMessage = "";
+	protected Color outputColor = null;
 
 	/**
-	 * Command constructor method.
+	 * Creates a new {@link Command} object and adds it to the {@link CommandsManager} list.
+	 */
+	public Command()
+	{
+		CommandsManager.add(this);
+	}
+
+	/**
+	 * Gets this command as a String.
 	 * 
-	 * @param handler
-	 *            Your command handler.
-	 * @param command
-	 *            The command in string form (e.g. "/test").
-	 * @param help
-	 *            Usage or any other help message (e.g.
-	 *            "Prints a test message").
-	 * @param minParams
-	 *            The minimum number of parameters one is allowed to send.
-	 * @param maxParams
-	 *            The maximum number of parameters one is allowed to send (-1
-	 *            for infinity).
-	 */
-	public Command(T handler, String command, String help, int minParams, int maxParams)
-	{
-		this.handler = handler;
-		this.command = command;
-		this.help = help;
-		this.containsHTML = false;
-		this.minParams = minParams;
-		this.maxParams = maxParams;
-	}
-
-	/**
-	 * Add a command to the command list.
-	 * 
-	 * @param command
-	 *            The command you'd like to add.
-	 */
-	public static void add(Command<?> command)
-	{
-		if (getCommand(command.getCommand()) == null)
-			commands.add(command);
-		else
-			throw new IllegalStateException(command.getCommand() + " already exists in the list!");
-	}
-
-	/**
-	 * @return The message of this command.
-	 */
-	public String getMessage()
-	{
-		return message;
-	}
-
-	/**
-	 * @return The help/usage message of this command.
-	 */
-	public String getHelp()
-	{
-		return help;
-	}
-
-	/**
 	 * @return The command as a String.
 	 */
-	public String getCommand()
-	{
-		return command;
-	}
+	public abstract String getCommand();
 
 	/**
-	 * Whether or not the message contains HTML. Use this when you're printing
-	 * your command messages in an HTML compatible text area.
+	 * Gets this command's help message.
 	 * 
-	 * @return True if the message contains HTML, else false.
+	 * @return This commands's help message.
 	 */
-	public boolean containsHTML()
-	{
-		return this.containsHTML;
-	}
+	public abstract String getHelpMessage();
 
 	/**
-	 * Will return a String with all the commands and their help/usage messages.
+	 * Gets the proper usage of this command.
 	 * 
-	 * @param HTML
-	 *            True if you want it formatted in HTML, false if not.
-	 * @return A String with all the command and their help/usage messages. If
-	 *         no commands are in the list "No commands" will be returned.
+	 * @return The proper usage of this command.
 	 */
-	public static String getAllHelp(boolean HTML)
-	{
-		if (commands.size() == 0)
-			return "No commands";
-		if (HTML)
-		{
-			String help = "<b><u>Commands:</u></b><br>";
-			for (int i = 0; i < commands.size(); i++)
-			{
-				help += "&nbsp;&nbsp;- - -<br>";
-				help += "&nbsp;&nbsp;" + commands.get(i).getCommand() + "<br>";
-				help += "&nbsp;&nbsp;" + commands.get(i).getHelp() + "<br>";
-			}
-			help += "&nbsp;&nbsp;- - -";
-			return help;
-		}
-		String help = "Commands:\n";
-		for (int i = 0; i < commands.size(); i++)
-		{
-			help += "- - -\n";
-			help += commands.get(i).getCommand() + "\n";
-			help += commands.get(i).getHelp() + "\n";
-		}
-		help += "- - -";
-		return help;
-	}
+	public abstract String getUsage();
 
 	/**
-	 * This method will take your parameters String array and combine all the
-	 * indexes into one String object.
+	 * Gets the maximum number of parameters this command can be used with. -1 for infinite.
 	 * 
-	 * @param params
-	 *            The String array of parameters.
-	 * @param start
-	 *            The index with which you'd like to start combining.
-	 * @return A String object which contains all the parameters combined.
+	 * @return The maximum number of parameters this command can be used with. -1 for infinite.
 	 */
-	public static String stringParams(String[] params, int start)
-	{
-		if (params.length == 0 || start >= params.length)
-			return null;
-		String temp = params[start];
-		for (int i = start + 1; i < params.length; i++)
-			temp += " " + params[i];
-		return temp;
-	}
+	public abstract int getMaxParams();
 
 	/**
-	 * Will parse a command from an input String. For example:
-	 * "/test param1 param2" will return "/test".
+	 * Gets the minimum number of parameters this command can be used with.
 	 * 
-	 * @param command
-	 *            The input to parse.
-	 * @return The parsed command.
+	 * @return The minimum number of parameters this command be used with.
 	 */
-	public static String parseCommand(String command)
-	{
-		if (command.trim().contains(" "))
-			return command.trim().substring(command.trim().indexOf(0)=='/'?command.indexOf("/"):0, command.indexOf(" ")).trim();
-		return command;
-	}
+	public abstract int getMinParams();
 
 	/**
-	 * Will parse the parameters from an input String into a String array.
-	 * 
-	 * For example: "/test param1 param2" will return {"param1", "param2}.
-	 * 
-	 * @param command
-	 *            The input to parse.
-	 * @return The parsed parameters.
-	 */
-	public static String[] parseParams(String command)
-	{
-		command = command.trim();
-		int count = 0;
-		for (int i = 0; i < command.length(); i++)
-			if (command.charAt(i) == ' ')
-			{
-				count++;
-				while (i < command.length() && command.charAt(i) == ' ')
-					i++;
-			}
-		if (count == 0)
-			return new String[] {};
-		command += " ";
-		String[] params = new String[0];
-		String temp = null;
-		for (int i = 0; i < command.length(); i++)
-		{
-			if (command.charAt(i) == ' ')
-			{
-				if (temp != null)
-				{
-					String[] temp2 = new String[params.length + 1];
-					for (int j = 0; j < params.length; j++)
-						temp2[j] = params[j];
-					temp2[temp2.length - 1] = temp;
-					params = temp2;
-				}
-				temp = null;
-				continue;
-			}
-			if (temp == null)
-				temp = "";
-			temp += command.charAt(i);
-		}
-		String[] temp2 = new String[params.length - 1];
-		for (int i = 1; i < params.length; i++)
-			temp2[i - 1] = params[i];
-		params = temp2;
-		return params;
-	}
-
-	/**
-	 * Possibly the most important method. Use this method to get a command
-	 * object using String from the command list.
-	 * 
-	 * @param command
-	 *            The command you'd like to get (e.g. "/test")
-	 * @return The command object corresponding to the provided String. Null if
-	 *         not found.
-	 */
-	public static Command<?> getCommand(String command)
-	{
-		for (Command<?> temp : commands)
-			if (temp.getCommand().equals(command))
-				return temp;
-		return null;
-	}
-
-	public String toString()
-	{
-		return "Command: " + this.command + "\nMessage: " + this.message + "\nHelp: " + this.help;
-	}
-
-	/**
-	 * This method will check the validity of the parameters and execute it if
-	 * the check is successful.
-	 * 
-	 * @param params
-	 *            A String array of the parameters you'd like to execute this
-	 *            command with.
-	 */
-	public void checkAndExecute(String[] params)
-	{
-		this.message="";
-		try
-		{
-			if (check(this.handler, params))
-				execute(this.handler, params);
-		}
-		catch (Exception e)
-		{
-			this.message = "[COMMAND ERROR] Problem with check method! (Consider making an override for your command)";
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * The method that checks the validity of a parameters String array for this
-	 * command.
-	 * 
-	 * You should never really have to explicitly call this method. Use
-	 * {@link #checkAndExecute(String[])} instead.
+	 * Gets the color that this command's output text should be printed with.
 	 * <p>
-	 * The default check method simply checks if the length of the String array
-	 * of parameters falls within the bounds of the {@link #minParams} and
-	 * {@link #maxParams} of this command. You could override it with your own
-	 * class if it's creating issues.
+	 * For example, suppose the command's output text indicates that some error has occurred, the color returned would be red. <br>
+	 * This color doesn't matter unless this command's output is being printed with a colored text compatible handler.
 	 * 
-	 * @param handler
-	 *            The command handler you're working with.
+	 * @return The color that this command's output text should be printed with.
+	 */
+	public Color getOutputColor()
+	{
+		return this.outputColor;
+	}
+
+	/**
+	 * This method will check the validity of the parameters and execute it if the check is successful.
+	 * 
+	 * @param params
+	 *            A String array of the parameters you'd like to execute this command with.
+	 */
+	public String checkAndExecute(String[] params)
+	{
+		this.outputColor = null;
+		this.outputMessage = "";
+		if (check(params))
+			execute(params);
+		return this.outputMessage;
+	}
+
+	/**
+	 * The method that checks the validity of a parameters String array for this command.<br>
+	 * You should never really have to explicitly call this method. Use {@link #checkAndExecute(String[])} instead.
+	 * <p>
+	 * The default check method simply checks if the length of the String array of parameters falls within the bounds of the {@link #minParams} and {@link #maxParams} of this command.
+	 * 
 	 * @param params
 	 *            A String array of parameters.
 	 * @return True if the check is successful, else false.
 	 */
-	public boolean check(T handler, String[] params)
+	protected boolean check(String[] params)
 	{
-		if (params.length >= this.minParams && ((this.maxParams >= 0) ? params.length <= this.maxParams : true))
+		if (params.length >= getMinParams() && ((getMaxParams() >= 0) ? params.length <= getMaxParams() : true))
 			return true;
-		if (params.length < this.minParams)
-			missingParameters(handler, params);
+		if (params.length < getMinParams())
+			missingParameters(params);
 		else
-			if (params.length > this.maxParams)
-				tooManyParameters(handler, params);
+			if (params.length > getMaxParams())
+				tooManyParameters(params);
 		return false;
 	}
 
 	/**
-	 * This method gets called if the check method finds that the parameter
-	 * array provided contains too many parameters.
-	 * 
-	 * The idea is that you can override this method to write your own code for
-	 * these situations.
-	 * 
-	 * @param handler
-	 *            The command handler you're working with.
-	 * @param params
-	 *            A String array of parameters.
-	 */
-	public void tooManyParameters(T handler, String[] params)
-	{
-		this.message = "Too many parameters!";
-	}
-
-	/**
-	 * This method gets called if the check method finds that the parameter
-	 * array provided contains too little parameters.
-	 * 
-	 * The idea is that you can override this method to write your own code for
-	 * these situations.
-	 * 
-	 * @param handler
-	 *            The command handler you're working with.
-	 * @param params
-	 *            A String array of parameters.
-	 */
-	public void missingParameters(T handler, String[] params)
-	{
-		this.message = "Missing parameters!";
-	}
-
-	/**
-	 * The execute code.
-	 * 
-	 * You should never really have to explicitly call this method. Use
-	 * {@link #checkAndExecute(String[])} instead.
+	 * The execute code.<br>
+	 * You should never really have to explicitly call this method. Use {@link #checkAndExecute(String[])} instead.
 	 * <p>
-	 * Simply put whatever code you'd like to be run in here, and when this
-	 * command is executed the code will be run.
+	 * Simply put whatever code you'd like to be run in here, and when this command is executed the code will be run.
 	 * 
 	 * @param handler
 	 *            The command handler you're working with.
 	 * @param params
 	 *            A String array of parameters.
 	 */
-	public abstract void execute(T handler, String[] params);
+	protected abstract void execute(String[] params);
 
 	/**
-	 * This method receives a String array of parameters which contain flags (which are formatted "flag:value") and will create a HashMap<String, String> which contains the flags as keys and their values.
-	 * @param params String array of parameters where each place contains a flag.
-	 * @return A HashMap<String, String> which contains the flags and their values.
+	 * This method gets called if the check method finds that the parameter array provided contains too little parameters.
+	 * <p>
+	 * By default it sets this command's output message to "Missing parameters!" and this command's output color to red.
+	 * 
+	 * @param params
+	 *            A String array of parameters.
 	 */
-	public static HashMap<String, String> parseFlags(String[] params)
+	protected void missingParameters(String[] params)
 	{
-		HashMap<String, String> flags=new HashMap<String, String>();
-		for(String param:params)
-		{
-			Scanner sc=new Scanner(param);
-			sc.useDelimiter(":");
-			try
-			{
-				flags.put(sc.next().trim(), sc.next().trim());
-			}
-			catch(Exception e)
-			{
-				throw new IllegalStateException("Incorrect syntax!");
-			}
-		}
-		return flags;
+		this.outputMessage = "Missing parameters!";
+		this.outputColor = Color.red;
+	}
+
+	/**
+	 * This method gets called if the check method finds that the parameter array provided contains too many parameters.
+	 * <p>
+	 * By default it sets this command's output message to "Too many parameters!" and this command's output color to red.
+	 * 
+	 * @param params
+	 *            A String array of parameters.
+	 */
+	protected void tooManyParameters(String[] params)
+	{
+		this.outputMessage = "Too many parameters!";
+		this.outputColor = Color.red;
+	}
+
+	/**
+	 * Returns a String containing information about this command using the following format:<br>
+	 * [This command as a String]: [This command's help message or "<no help message specified>" if none specified], Usage: [This command's usage or "<none specified>" if none specified]
+	 * 
+	 * @return A String containing information about this command.
+	 */
+	public String toString()
+	{
+		return getCommand() + ": " + (getHelpMessage().length() == 0 ? "<no help message specified>" : getHelpMessage()) + ", Usage: " + (getUsage().length() == 0 ? "<none specified>" : getUsage());
 	}
 }
---
+++ src/com/moomoohk/MooCommands/InvalidCommandException.java
@@ -0,0 +1,24 @@
+package com.moomoohk.MooCommands;
+
+/**
+ * Thrown when an invalid {@link Command} is created.
+ * 
+ * @author Meshulam Silk <moomoohk@ymail.com>
+ * @version 1.0
+ * @since 2013-11-24
+ */
+public class InvalidCommandException extends Exception
+{
+	private static final long serialVersionUID = -1529318301670175287L;
+
+	/**
+	 * Constructs an {@link InvalidCommandException} with the specified detail message.
+	 * 
+	 * @param message
+	 *            The detail message.
+	 */
+	public InvalidCommandException(String message)
+	{
+		super(message);
+	}
+}
