c4d2800638146d6d5236db5a6f624bef44cafb40
Meshulam Silk <moomoohk@ymail.com>
2014-09-27 12:10:42 +0300

    Refreshed .gitignore

---
+++ src/com/moomoohk/MooCommands/example/HelpCommand.java
@@ -0,0 +1,74 @@
+package com.moomoohk.MooCommands.example;
+
+import java.awt.Color;
+
+import com.moomoohk.MooCommands.Command;
+import com.moomoohk.MooCommands.CommandsManager;
+
+/**
+ * 
+ * @author Meshulam Silk (moomoohk@ymail.com)
+ * @since Jul 17, 2014
+ */
+public class HelpCommand extends Command
+{
+	public HelpCommand()
+	{
+		super();
+	}
+
+	@Override
+	public boolean check(String[] params)
+	{
+		if (params.length > 0 && CommandsManager.findCommand(params[0]) == null)
+		{
+			this.outputMessage = "Command not found!";
+			this.outputColor = Color.red;
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void execute(String[] params)
+	{
+		if (params.length == 0)
+		{
+			this.outputMessage = "---";
+			for (Command c : CommandsManager.getAllCommands())
+				this.outputMessage += "\n" + c.toString() + "\n---";
+			return;
+		}
+		this.outputMessage = CommandsManager.findCommand(params[0]).toString();
+	}
+
+	@Override
+	public String getCommand()
+	{
+		return "help";
+	}
+
+	@Override
+	public String getHelpMessage()
+	{
+		return "Shows help";
+	}
+
+	@Override
+	public String getUsage()
+	{
+		return "help [command]";
+	}
+
+	@Override
+	public int getMaxParams()
+	{
+		return 1;
+	}
+
+	@Override
+	public int getMinParams()
+	{
+		return 0;
+	}
+}
---
+++ src/com/moomoohk/MooCommands/example/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package which contains an example Command class
+ */
+/**
+ * @author Meshulam Silk (moomoohk@ymail.com)
+ *	@since Jul 17, 2014
+ */
+package com.moomoohk.MooCommands.example;
